011b963021d72e2db1d23a2a0fd4003aee4d6b48
Valmir Correa <valmir.correa@ufrn.edu.br>
2019-08-19 22:21:48 -0300

    analisando git

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 'use strict'
 
-console.log('Here\'s a hidden message')
-
 // Gerar data e coloca em um id
 let today = new Date()
 let formatDate = today.toDateString()
 let selectElement = document.getElementById('date')
 selectElement.innerHTML = formatDate
+
+console.log('Here\'s a hidden message')
